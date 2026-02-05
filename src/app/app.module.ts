31cc55fda0434992bcb22341e069b10fabb28d07
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-02-05 14:59:44 -0300

    NEST - Criado controller manual e automatico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConceitosAutomaticoModule } from 'src/conceitos-automatico/conceitos-automatico.module';
+import { ConceitosManualModule } from 'src/conceitos-manual/conceitos-manual.module';
 
 // um módulo pode exportar um módulo raiz em outro módulo
 @Module({
-  imports: [ConceitosAutomaticoModule],
+  imports: [ConceitosAutomaticoModule, ConceitosManualModule],
   controllers: [AppController], // controla request e response
   providers: [AppService], // são utilizados para colocar a regra de negócio da aplicação
 })
