d082eb2afc614570f7bc3a6915e1c9561de0586e
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2026-02-07 15:31:44 -0300

    NEST - Excluindo modulos temporarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConceitosAutomaticoModule } from 'src/conceitos-automatico/conceitos-automatico.module';
-import { ConceitosManualModule } from 'src/conceitos-manual/conceitos-manual.module';
 
 // um módulo pode exportar um módulo raiz em outro módulo
 @Module({
-  imports: [ConceitosAutomaticoModule, ConceitosManualModule],
+  imports: [],
   controllers: [AppController], // controla request e response
   providers: [AppService], // são utilizados para colocar a regra de negócio da aplicação
 })
