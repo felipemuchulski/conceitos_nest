04d1d4c5604fef1528bd09c8272d7269e4927f81
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2026-02-11 18:59:45 -0300

    feat: Criado post e dto para o método post

---
+++ src/recados/dtos/create-recado.dto.ts
@@ -0,0 +1,4 @@
+export class CreateRecadoDTO {
+  recado: string;
+  autor: string;
+}
--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Body, Controller, HttpCode, HttpStatus, Get, Param, Post } from '@nestjs/common';
+import { CreateRecadoDTO } from './dtos/create-recado.dto';
 @Controller('recados')
 export class RecadosController {
   @Get('all_recados')
+  @HttpCode(HttpStatus.OK)
   findAll() {
     return 'Essa retorna um recado';
   }
 
   @Get(':id')
-  findOne() {
+  findOne(@Param('id') id: string) {
+    console.log(id);
     return 'Essa rota retorna um parametro';
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() recado: CreateRecadoDTO) {
+    console.log(recado);
+    return recado;
   }
 }
