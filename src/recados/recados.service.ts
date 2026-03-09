a444714f43c29f980542889abd79cb977ba250d8
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-03-09 15:06:48 -0300

    fix: Colocado pagination com o Query

--- src/recados/recados.service.ts
+++ src/recados/recados.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Recado } from './entities/recado.entity';
 import { Repository } from 'typeorm';
 import { PessoasService } from '../pessoas/pessoas.service';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class RecadosService {
@@ -53,6 +54,31 @@ export class RecadosService {
 
   async findAll() {
     const recados = await this.recadoRepository.find();
+    return recados;
+  }
+
+  async findAllPagination(paginationDto?: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto ?? {};
+
+    const recados = await this.recadoRepository.find({
+      take: limit,
+      skip: offset,
+      relations: ['de', 'para'],
+      order: {
+        id: 'desc',
+      },
+      select: {
+        de: {
+          id: true,
+          nome: true,
+        },
+        para: {
+          id: true,
+          nome: true,
+        },
+      },
+    });
+
     return recados;
   }
 
