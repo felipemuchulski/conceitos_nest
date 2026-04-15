51c83aa28ee23ee2425617155ffa5a2b2af1ad0e
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-04-15 15:17:00 -0300

    fix: Ajustado controller

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,18 +1,26 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Param, Controller, Get, Post, UseGuards, UseInterceptors, Req } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { IsAuthenticatedGuard } from '@/common/guards/is-authenticated.guard';
 import { OwnsPaymentGuard } from './guards/owns-payment.guard';
+import { TransformPaymentResponseInterceptor } from './interceptors/transform-payment-response.interceptor';
+import type { Request as ExpressRequest } from 'express';
 
 @Controller('payments')
 @UseGuards(IsAuthenticatedGuard, OwnsPaymentGuard)
+@UseInterceptors(TransformPaymentResponseInterceptor)
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentService) {}
 
-  @Post('charge')
-  create(@Body() createPaymentDto: CreatePaymentDto, userId: number) {
-    return this.paymentsService.createCharge(createPaymentDto, userId);
+  @Post()
+  create(@Body() createPaymentDto: CreatePaymentDto, @Req() req: ExpressRequest) {
+    return this.paymentsService.createCharge(createPaymentDto, req.user!.id);
   }
 
   // Necessário criar os outros métodos
+  @Get(':reference')
+  @UseInterceptors(TransformPaymentResponseInterceptor)
+  findbyReference(@Param('reference') reference: string) {
+    return this.paymentsService.findByReference(reference);
+  }
 }
